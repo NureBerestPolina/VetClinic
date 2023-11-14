32c6d4aa4216af2f7747747c6bbb0f4e3a3fcfbf
Polina Berest <pamapolya@gmail.com>
2023-11-14 19:45:10 +0200

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { StaffComponent } from './features/staff/staff.component';
+import { ProceduresComponent } from './features/procedures/procedures.component';
+import { AnimalsComponent } from './features/animals/animals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'staff',
+    component: StaffComponent
+  },
+  {
+    path:'procedures',
+    component: ProceduresComponent
+  },
+  {
+    path:'animals',
+    component: AnimalsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { AminalsComponent } from './features/aminals/aminals.component';
+import { StaffComponent } from './features/staff/staff.component';
+import { ProceduresComponent } from './features/procedures/procedures.component';
+import { AnimalsComponent } from './features/animals/animals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AminalsComponent,
+    StaffComponent,
+    ProceduresComponent,
+    AnimalsComponent
   ],
   imports: [
     BrowserModule,
